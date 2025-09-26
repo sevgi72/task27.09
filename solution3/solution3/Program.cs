36e9c28af24e43a3fd2f56f4949748e83ef7bda6
sevgi72 <sevgimf@code.edu.az>
2025-09-26 20:34:28 +0400

    add files

---
+++ solution3/solution3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num = 8;
+        int numtest = num;
+        while (numtest % 2 == 0 && numtest > 1)
+        {
+            numtest /= 2;
+        }
+        if (numtest == 1)
+        {
+            Console.WriteLine("ikinin quvvetidir");
+        }
+        else
+            Console.WriteLine("ikinin quvveti deyil");
+
+    }
+}
---
+++ solution4/solution4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int num = 1;
+        int numcopy = num;
+        int count = 0;
+
+        while (numcopy > 0)
+        {
+            if (num % numcopy == 0)
+                count++;
+            numcopy -= 1;
+        }
+
+        if (count > 2)
+        {
+            Console.WriteLine("eded murekkebdir");
+        }
+        else if (count == 2)
+        {
+            Console.WriteLine("eded sadedir");
+        }
+        else
+        {
+            Console.WriteLine("ne sade ne de murekkebdir");
+        }
+    }
+}
