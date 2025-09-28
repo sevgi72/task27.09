e57aca01a6add2eca146c0ea216401feeaa32f95
sevgi72 <sevgimf@code.edu.az>
2025-09-29 00:04:13 +0400

    add files

---
+++ solution.1/solution.1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        int sum = 0;
+        int count = 0;
+        while (count < 100)
+        {
+            sum = sum + count;
+            count = count + 2;
+        }
+
+        Console.WriteLine(sum);
