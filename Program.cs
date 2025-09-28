e4c445ba9f1e96b7ae8bd2d0f78104aef91e882b
sevgi72 <sevgimf@code.edu.az>
2025-09-29 00:18:04 +0400

    edit:fixed git branches

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int copy = 0;
+            while (num != 0)
+            {
+                copy = num % 10;
+                Console.WriteLine(copy);
+                num /= 10;
+                
+            }
+
+        }
+
+    }
+}
